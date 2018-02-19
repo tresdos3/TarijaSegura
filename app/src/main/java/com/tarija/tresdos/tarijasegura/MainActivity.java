ddf0bd2b471ca466565fec9428c1c3c0887fd01b
Luis Juarez <tresdosasd@gmail.com>
2018-02-19 09:01:55 -0400

    Add firebase database to main for child

--- app/src/main/java/com/tarija/tresdos/tarijasegura/MainActivity.java
+++ app/src/main/java/com/tarija/tresdos/tarijasegura/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,19 +17,35 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tarija.tresdos.tarijasegura.fragments.DashboardFragment;
 import com.tarija.tresdos.tarijasegura.fragments.NewChildFragment;
 import com.valdesekamdem.library.mdtoast.MDToast;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout l1;
+    private ConstraintLayout c1;
+
+    private TextView father_ci, father_mail, father_phone, father_name, father_family, child_name;
+
     private FirebaseUser user;
+    private DatabaseReference rootRef, profileRef, childRef;
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -38,16 +55,30 @@ public class MainActivity extends AppCompatActivity {
     public static final String mypreference = "mypref";
     public static final String Tipo = "tipoKey";
     public static final String Session = "SessionKey";
+    public static final String Huid = "HuidKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading);
 
+        l1 = (LinearLayout) findViewById(R.id.ll);
+        c1 = (ConstraintLayout) findViewById(R.id.cl);
+
+        father_name = (TextView) findViewById(R.id.father_name);
+        father_ci = (TextView) findViewById(R.id.father_ci);
+        father_mail = (TextView) findViewById(R.id.father_mail);
+        father_phone = (TextView) findViewById(R.id.father_phone);
+        father_family = (TextView) findViewById(R.id.father_family);
+        child_name = (TextView) findViewById(R.id.child_name);
+
         sharedPreferences = getSharedPreferences(mypreference,
                 Context.MODE_PRIVATE);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        profileRef = rootRef.child(user.getUid());
+        childRef = rootRef.child(user.getUid()).child("/hijos");
 
         if (user != null){
             String type = sharedPreferences.getString(Tipo, "");
@@ -176,4 +207,55 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private void RegisterToken(){}
+    private void ViewProfile() {
+        c1.setVisibility(View.GONE);
+        l1.setVisibility(View.VISIBLE);
+    }
+    private void HideProfile() {
+        c1.setVisibility(View.VISIBLE);
+        l1.setVisibility(View.GONE);
+    }
+    private void ChargeProfile() {
+        HideProfile();
+        profileRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    profileRef.child("profile").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            father_name.setText(dataSnapshot.child("name").getValue().toString() + " " + dataSnapshot.child("lastname").getValue().toString());
+                            father_ci.setText(dataSnapshot.child("ci").getValue().toString());
+                            father_mail.setText(dataSnapshot.child("email").getValue().toString());
+                            father_phone.setText(user.getPhoneNumber());
+                            father_family.setText(dataSnapshot.child("family").getValue().toString());
+                            String uid = sharedPreferences.getString(Huid,"");
+                            childRef.child(uid).addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    Map<String, String> map = (Map)dataSnapshot.getValue();
+                                    child_name.setText(map.get("nombre"));
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                    messages(MDToast.TYPE_ERROR,"Algo salio mal :(");
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            messages(MDToast.TYPE_ERROR,"Algo salio mal :(");
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                messages(MDToast.TYPE_ERROR,"Esto es vergonzoso :)");
+            }
+        });
+    }
 }
